960d8d93caacdc1ce8ff38dcfd400f1fc5eaab0e
BennyWang1007 <bennywang1007@gmail.com>
2025-01-14 21:36:57 +0800

    update EEPROM utils

--- EEPROM/eeprom.c
+++ EEPROM/eeprom.c
@@ -1,8 +1,7 @@
 #include "eeprom.h"
 
 void EEPROM_init() {
-    EECON1 = 0x00;
-    EECON1bits.WREN = 1;
+    PIE2bits.EEIE = 0;
 }
 
 void EEPROM_write(uint8_t address, uint8_t data) {
@@ -11,18 +10,21 @@ void EEPROM_write(uint8_t address, uint8_t data) {
     EECON1bits.EEPGD = 0;   // access data EEPROM memory
     EECON1bits.CFGS = 0;
     EECON1bits.WREN = 1;
+    INTCONbits.GIE = 0;
     EECON2 = 0x55;
     EECON2 = 0xAA;
     EECON1bits.WR = 1;
-    PIR2.EEIE = 0;
-    while (EECON1bits.WR);
+    INTCONbits.GIE = 1;
+    while(EECON1bits.WR == 1);
+    PIR2bits.EEIF = 0;
     EECON1bits.WREN = 0;
 }
 
 uint8_t EEPROM_read(uint8_t address) {
     EEADR = address;
-    EECON1bits.EEPGD = 0;
-    EECON1bits.CFGS = 0;
+    EECON1bits.WREN = 0;	/* WREN bit is clear for Read operation*/  
+    EECON1bits.EEPGD = 0;	/* Access data EEPROM memory*/
     EECON1bits.RD = 1;
+    PIR2bits.EEIF = 0;
     return EEDATA;
 }
