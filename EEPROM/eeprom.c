a9b34c74e562e5736ed512c03418ef164cbcc744
nyraa <112930946+nyraa@users.noreply.github.com>
2025-01-14 21:38:13 +0800

    update EEPROM util by doc

--- EEPROM/eeprom.c
+++ EEPROM/eeprom.c
@@ -9,20 +9,20 @@ void EEPROM_write(uint8_t address, uint8_t data) {
     EEADR = address;
     EEDATA = data;
     EECON1bits.EEPGD = 0;   // access data EEPROM memory
-    EECON1bits.CFGS = 0;
-    EECON1bits.WREN = 1;
+    EECON1bits.CFGS = 0;    //  program memory/data EEPROM memory
+    EECON1bits.WREN = 1;    // allow write operation
     EECON2 = 0x55;
     EECON2 = 0xAA;
-    EECON1bits.WR = 1;
+    EECON1bits.WR = 1;  // start write operation
     PIR2.EEIE = 0;
     while (EECON1bits.WR);
-    EECON1bits.WREN = 0;
+    EECON1bits.WREN = 0;    // disable write operation
 }
 
 uint8_t EEPROM_read(uint8_t address) {
     EEADR = address;
-    EECON1bits.EEPGD = 0;
-    EECON1bits.CFGS = 0;
-    EECON1bits.RD = 1;
+    EECON1bits.EEPGD = 0;   // access data EEPROM memory
+    EECON1bits.CFGS = 0;    //  program memory/data EEPROM memory
+    EECON1bits.RD = 1;  // start read operation
     return EEDATA;
 }
