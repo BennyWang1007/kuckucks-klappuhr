9bd7e6b8d531e75be21c3bdb5541756e8b9d58d8
nyraa <112930946+nyraa@users.noreply.github.com>
2025-01-14 21:43:20 +0800

    update EEPROM utils comment

--- EEPROM/eeprom.c
+++ EEPROM/eeprom.c
@@ -8,16 +8,16 @@ void EEPROM_write(uint8_t address, uint8_t data) {
     EEADR = address;
     EEDATA = data;
     EECON1bits.EEPGD = 0;   // access data EEPROM memory
-    EECON1bits.CFGS = 0;    //  program memory/data EEPROM memory
+    EECON1bits.CFGS = 0;    // select program memory/data EEPROM memory
     EECON1bits.WREN = 1;    // allow write operation
-    INTCONbits.GIE = 0;
-    EECON2 = 0x55;
+    INTCONbits.GIE = 0;     // disable global interrupt during write operation
+    EECON2 = 0x55;  // write sequence must be followed, no interrupt between these instructions
     EECON2 = 0xAA;
     EECON1bits.WR = 1;  // start write operation
-    INTCONbits.GIE = 1;
+    INTCONbits.GIE = 1; // re-enable global interrupt
     while(EECON1bits.WR == 1);
-    PIR2bits.EEIF = 0;
-    EECON1bits.WREN = 0;    // disable write operation
+    PIR2bits.EEIF = 0;  // clear EEIF flag
+    EECON1bits.WREN = 0;    // disable write operation after write
 }
 
 uint8_t EEPROM_read(uint8_t address) {
