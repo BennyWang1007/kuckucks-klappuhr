042e6fc43535e865cd758fabd400c44fd208fed0
nyraa <112930946+nyraa@users.noreply.github.com>
2025-01-14 15:04:28 +0800

    add step motor deg calculation

--- utils.c
+++ utils.c
@@ -9,6 +9,7 @@
 #include <xc.h>
 
 #include <stdlib.h>
+#include "step_motor/step_motor.h"
 
 void reverse(char* str, int length)
 {
@@ -50,4 +51,24 @@ char itoa(unsigned int num, unsigned char* str, int len)
     reverse((char*)str, i);
 
     return 0;
+}
+
+uint16_t min2step(uint32_t min)   // min in 0 ~ 1440(1 day)
+{
+    uint16_t step = min * MOTOR_STEP / 1440;
+    return step;
+}
+
+uint16_t timediff_in_min(uint8_t h1, uint8_t m1, uint8_t h2, uint8_t m2)
+{
+    uint16_t diff = 0;
+    uint16_t t1 = h1 * 60 + m1;
+    uint16_t t2 = h2 * 60 + m2;
+
+    if (t2 >= t1)   // new time after old time in same day
+        diff = t2 - t1;
+    else    // new time in next day
+        diff = t1 + 1440 - t1;
+
+    return diff;
 }
